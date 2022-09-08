0eab4d8c64b21de5ecda51aa9e8ab8484f34826b
Eugene Ncube <philosncube@gmail.com>
2022-09-08 21:50:21 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * This script prints out a string
  * without using printf
  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
+	puts (str);
 	return (0);
 }

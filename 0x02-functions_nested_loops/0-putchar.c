3755f4e467688e2f7fb56bfec03221fed38b8f62
Eugene Ncube <philosncube@gmail.com>
2022-09-13 21:19:42 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar in lowercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char put[] = ("_putchar \n");
+
+	printf("%s", put);
+	return (0);
+}

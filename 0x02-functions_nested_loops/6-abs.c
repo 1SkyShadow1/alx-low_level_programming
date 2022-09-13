f695b5c146fcbfce0df664aac64d6b31ca46c07f
Eugene Ncube <philosncube@gmail.com>
2022-09-13 23:36:50 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _abs - main function
+* @n: integer to make absolute
+* Return: int or int times negative
+*/
+int _abs(int num)
+{
+	if (num > 0)
+	return (num);
+	else
+	return (num * -1);
+}

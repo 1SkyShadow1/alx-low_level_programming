44613582bfb175d6bb2c923a444ae5af814b5b7e
Eugene Ncube <philosncube@gmail.com>
2022-09-13 23:04:23 +0000

    alphabet10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+* print_alphabet_x10 - prints alphabet 10 times
+* Return: always 0;
+*/
+void print_alphabet_x10(void)
+{
+	char tenalpha
+
+	for (tenalpha = '0'; tenalpha < '10'; tenalpha++)
+	{
+	print_alphabet(tenalpha);
+	}
+	_putchar('\n');
+}

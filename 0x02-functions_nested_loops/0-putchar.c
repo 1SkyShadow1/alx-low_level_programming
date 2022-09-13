6906919152be67b68d0ccbdead794f684d3bc5b3
Eugene Ncube <philosncube@gmail.com>
2022-09-13 22:17:32 +0000

    head

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Prints _putchar in lowercase
- * Return: Always 0.
- */
+* main - Prints _putchar in lowercase
+* Return: Always 0.
+*/
 int main(void)
 {
 	_putchar('_');
@@ -13,5 +13,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

6144e35033ac50262238636245bce13c53a9e6ad
Eugene Ncube <philosncube@gmail.com>
2022-09-14 00:02:08 +0000

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
 /**
-* jack_bauer - time
+* jack_bauer - main thing
 */
 void jack_bauer(void)
 {
-	int alpha;
-	int j:
-	
-	for (alpha = 0; alpha <= 23; alpha++)
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
 	{
 	for (j = 0; j <= 59; j++)
-	{
-	_putchar(alpha / 10 + '0');
-	_putchar(alpha % 10 + '0');
+	{		
+	_putchar(i / 10 + '0');
+	_putchar(i % 10 + '0');
 	_putchar(':');
 	_putchar(j / 10 + '0');
 	_putchar(j % 10 + '0');

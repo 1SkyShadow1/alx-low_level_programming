2a7dcd9e856935bfe177698c4fb453563bf87011
Eugene Ncube <philosncube@gmail.com>
2022-09-14 09:29:28 +0000

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+* times_table - prints a times table of 9
+* Retun: result
+*/
+void times_table(void)
+{
+	int n;
+	int i;
+	int result;
+
+	for (i = 0; i <= 9; ++i)
+	{
+	for (n = 9)
+	}
+	_putchar(n * i);
+	return (_putchar(n * i));
+	_putchar('\n');
+}

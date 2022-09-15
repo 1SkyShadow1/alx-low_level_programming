467be8671ca5a68c8324a50516c676ecd438df47
Eugene Ncube <philosncube@gmail.com>
2022-09-15 19:32:47 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+* _isupper - prints number 1 or 0 if c lower or uppercase
+* @c: character to compare
+* Return: Always 0;
+*/
+int _isupper(int c)
+{
+	char c;
+
+	if (c == 'C')
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
+	else if (c == 'c')
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+/**
+* _isupper - prints out number if upper or lowercase
+* @c: parameter to compare
+* Return: 1 or 0
+*/
+int _isupper(int c);
+
+#endif

25f9a55b42ee985606b7c65744d0518f3bbd009b
Eugene Ncube <philosncube@gmail.com>
2022-09-15 11:16:47 +0000

    checks and balances

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* mian - prints if 0 is zero
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	if (n == 0)
+	{
+	printf("%i is zero\n", n);
+	}
+	return (0);
+}	

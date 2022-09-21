5dd35c229ecebda434fdfef11e9375f2613b1fdc
Eugene Ncube <philosncube@gmail.com>
2022-09-21 20:04:37 +0000

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,13 @@ char *_strncat(char *dest, char *src, int n)
 	int b;
 
 	for (b = 0; dest[b] != '\0'; b++)
+	{}
+
+	for (a = 0; src[a] != '\0'; a++)
 	{
 	dest[b + a] = src[a];
 	}
 	dest[b + a] = '\0';
-
-	if (sizeof(src >= n))
-	{
-	(a = 0; src[a] != '\0'; a++)
-	dest[b + a] = src[a];
-	}
-	else 
-	src = NULL;
 
 	return (dest);
 }

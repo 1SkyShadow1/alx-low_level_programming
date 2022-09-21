5dcfa04274498a9b693f55f657bbc1272499e6be
Eugene Ncube <philosncube@gmail.com>
2022-09-21 19:37:34 +0000

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,13 @@ char *_strcat(char *dest, char *src)
 
 	for (b = 0; dest[b] != '\0'; b++)
 	{}
+
 	for (a = 0; src[a] != '\0'; a++)
 	{
 	dest[b + a] = src[a];
 	}
 	dest[b + a] = '\0';
+
+	return (dest);
+
 }

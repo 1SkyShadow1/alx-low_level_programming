d385e638f82417b9da7d26aee9d6ebbb5c0770c7
Eugene Ncube <philosncube@gmail.com>
2022-09-21 20:24:14 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,17 +9,11 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
-	int b;
-
-	*src = *dest;
-	for (b = 0; dest[b] != '\0'; b++)
-	{}
 
 	for (a = 0; a < n && src[a] != '\0'; a++)
 	{
-	dest[b + a] = src[a];
+	dest[a] = src[a];
 	}
-	dest[b + a] = '\0';
+	dest[a] = '\0';
 	return (dest);
-	}
 }

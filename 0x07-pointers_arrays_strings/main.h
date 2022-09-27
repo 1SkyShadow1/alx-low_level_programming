571860dae8e48482f8e8dddc55e63597801b8a42
Eugene Ncube <philosncube@gmail.com>
2022-09-27 23:23:13 +0000

    search

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -27,4 +27,12 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 */
 char *_strchr(char *s, char c);
 
+/**
+* _strspn - search a string for a set of bytes
+* @s: source string
+* @accept: accepted string
+* Return: number of bytes in the init segment
+*/
+unsigned int _strspn(char *s, char *accept);
+
 #endif

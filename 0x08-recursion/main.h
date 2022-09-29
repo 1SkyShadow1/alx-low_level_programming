27ab9b74353dfe328bfa3f98dc5bea7033dcd80a
Eugene Ncube <philosncube@gmail.com>
2022-09-29 20:36:32 +0000

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+/**
+* _puts_recursion - prints a string
+* @s: pointer to a char
+* Return: string
+*/
+void _puts_recursion(char *s);
+
+/**
+* _putchar - puts a character
+* @c: variable char
+* Return: Always 0
+*/
+void _putchar(char c);
+
+#endif

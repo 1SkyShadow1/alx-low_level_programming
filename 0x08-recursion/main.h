46bc708bad00adff17c70504f5000cce6c8636d7
Eugene Ncube <philosncube@gmail.com>
2022-09-29 21:00:14 +0000

    exponent

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -35,4 +35,13 @@ int _strlen_recursion(char *s);
 * Return: factorial of number
 */
 int factorial(int n);
+
+/**
+* _pow_recursion - returns the value of x raised to the power of y
+* @x: base
+* @y: exponent
+* return: value of the exponentiation
+*/
+int _pow_recursion(int x, int y);
+
 #endif

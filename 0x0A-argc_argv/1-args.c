334282d448a7c0739e70f7d0eb15c0b98cc5d29a
Eugene Ncube <philosncube@gmail.com>
2022-10-03 18:31:11 +0000

    counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - prints program name
+* @argc: number of prog lines
+* @argv: char in  prog
+* Return: name of prog
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

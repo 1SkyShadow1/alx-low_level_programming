e7606971ba887821728aa18fa975f721505887f1
Eugene Ncube <philosncube@gmail.com>
2022-10-11 16:03:51 +0000

    fixes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
-typedef struct dog {
+/**
+* struct dog - Typedef for dog
+* @name: name of dog
+* @owner: name of owner
+* @age: age of dog
+*/
+typedef struct dog
+{	char *name;
 
-	char *name;
 	char *owner;
+
 	float age;
 } dog;

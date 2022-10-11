3e08cebb1682b52cd665a382b8170fd8aa6179fa
Eugene Ncube <philosncube@gmail.com>
2022-10-11 19:15:32 +0000

    hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,9 +11,14 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!d)
+	struct dog my_dog;
+
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+
+	if (d == 0)
 		return;
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+
+	*d = my_dog;
 }

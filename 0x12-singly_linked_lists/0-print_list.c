917dc13b4b67b5b0008ebd68ae52f1cf983f5dde
Eugene Ncube <philosncube@gmail.com>
2022-10-21 19:51:03 +0000

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,25 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+
 /**
-* print_list - prints elements of list_t
-* @h: pointer to the head of the list
-* Return: number of nodes in list
+* print_list - Prints length and the string, then returns amount of nodes
+* @h: Pointer to a struct constant
+* Return: Returns amount of node
 */
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int i;
+	unsigned int count = 0;
 
-	tmp = h;
-	for (i = 0; tmp; i++)
+	while (h != NULL)
 	{
-	printf("[%u] %s\n", tmp->len, tmp->str);
-	tmp = tmp->next;
+	if (h->str == NULL)
+	printf("[%u] %s\n", 0, "(nil)");
+	else
+	printf("[%u] %s\n", h->len, h->str);
+
+	h = h->next;
+	count++;
 	}
-	return (i);
+
+	return (count);
 }

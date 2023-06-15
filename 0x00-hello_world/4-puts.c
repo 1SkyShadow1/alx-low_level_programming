68de3e9596d19eed65873e74a26b6aaae8fab079
Evans <Eazymoja5@gmail.com>
2023-06-15 13:17:58 -0700

    PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-/* This script prints out a string */
 /**
  * main - Entry point
- *   Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(str);
-
-	return (0);/* The script is successful */
+	puts=("/"Programming is like building a multilingual puzzle")
+	return (0);
 }

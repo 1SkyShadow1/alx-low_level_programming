f9f12efbd1792d062c43170f4822fa02a4453933
Evans <Eazymoja5@gmail.com>
2023-06-29 19:19:08 -0700

    Pointers and Arrays

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,15 +12,11 @@ void print_number(int n)
 		n = -n;  /* Convert negative number to positive*/
 	}
 
-	if (n >= 10)
+	if (n / 10 != 0 || n == 0)
 	{
 		print_number(n / 10);  /* Recursive call to print digits recursively*/
 	}
 
 	_putchar('0' + n % 10);  /* Print the current digit*/
-
-	if (n == 0 || n == -98)
-	{
-		_putchar('\n');  /* Print a newline character */
-	}
 }
+

e6d8feb792f7990840ba9a747408d7c4effd4cf4
Evans <Eazymoja5@gmail.com>
2023-06-30 07:05:58 -0700

    Pointers and Arrays

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - Changes array index to a new value
- * Return: Always 0
- */
 int main(void)
 {
 	int n;
@@ -20,7 +16,7 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 *        
 	 */
-	*(int*)((char*)p + 20) = 98;
+       	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
